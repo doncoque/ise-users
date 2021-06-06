cb25d62e83871c126054b62ddb3e74086ee416f4
Alvaro Orrego Brito <alvaro.orregobrito@gmail.com>
2021-06-05 21:47:51 -0500

    commenting and minor tweaks

--- ise-users.py
+++ ise-users.py
@@ -1,4 +1,5 @@
 import sys, getopt
+#from tabulate import tabulate
 import json
 import requests   
 from requests.auth import HTTPBasicAuth
@@ -24,15 +25,15 @@ def main(argv):
     elif opt in ("-l", "--list"):
       del_users = False
       arg_flag = True
-      print("Users will be listed...\n")
+      print("You chose only to list users.\n")
     elif opt in ("-d", "--delete"):
       del_users = True
       arg_flag = True
-      print("Users will be deleted...\n")
+      print("You chose to delete users form user.txt file.\n")
   if arg_flag :
     return del_users
   else : 
-    print("No arguments used. Use '-l' or '--list' to list all users on ISE, '-d' or '--delete' to delete the users on the users.txt file..")
+    print("No arguments used. Use '-l' or '--list' to list all users on ISE, '-d' or '--delete' to delete the users on the users.txt file.")
     sys.exit(1)
   
 def confirm(message):
@@ -47,7 +48,7 @@ def confirm(message):
   return answer == "y"   
 
 delete_enabled = False
-print("This scritp will either only list current users on ISE server or delete users provided on users.txt file\n")
+print("This scritp will either only list current users on ISE server or delete users provided on users.txt file.\n")
 delete_enabled = main(sys.argv[1:])
 
 # Open a file called "server.txt" with server ip/name and API key for ISE separated by newlines.
@@ -55,45 +56,49 @@ keyFile = open('server.txt', 'r')
 server = keyFile.readline().strip()
 api_key = keyFile.readline().strip()
 keyFile.close()
-print("Using server " + server)
-print("Using API Key " + api_key)
+print("Connecting to server " + server + " using API Key provided on file.")
 
+# Connecting to server to read all users.
 auth = HTTPBasicAuth("APIUser", api_key)
-
 headers = {'Accept': 'application/json',
            'Content-Type': 'application/json' }
-
 url = 'https://' + server + ':9060/ers/config/internaluser'
-
-response = requests.get(url, headers=headers, auth=auth, verify=False).json()
+try:
+  response = requests.get(url, headers=headers, auth=auth, verify=False).json()
+except:
+  print("Connection to server " + server + " failed. Exiting...")
+  sys.exit(2)
+    
+# Extracting user information from api response.
 users = response["SearchResult"]["resources"]
 
-# ISE returns by default 20 users per page. Here we cycle through the pages while the nextPage key is returned.
+# ISE returns by default 20 users per page. Here we cycle through the pages while the nextPage key is returned and append results to users.
 f = True
 while f:
   try:
     url = response["SearchResult"]["nextPage"]["href"]
-    print(url)
+    #print(url)
     response = requests.get(url, headers=headers, auth=auth, verify=False).json()
     users.extend(response["SearchResult"]["resources"])
   except:
     f = False
 
-# Here we extract user name and id  
+# Here we extract user name and id.
 user_list = {}
 for i, user in enumerate(users):
     user_list[user['name']] = user['id']
 
+# Delete was selected with arguments when the script was run.
 if delete_enabled:
 
   # Open a file called "users.txt" containing usernames (one on each newline) to be deleted.
   fo = open("users.txt", "r+")
-  print ("User list to be Deleting users in: ", fo.name)
+  print ("Deleting users contained in the file {}.".format(fo.name))
   line = fo.readlines()
   delete_list = []
   for i in line:
     delete_list.append(i.strip())
-  print ("Read Line: %s" % (delete_list))
+  print ("Users found on the file for deletion: {}".format(delete_list))
   # Close opened file
   fo.close()
 
@@ -104,21 +109,31 @@ if delete_enabled:
       delete_dic[user] = user_list[user]
     else:
       not_found.append(user)
-
   print("Users not found on ISE: {}".format(not_found))
   print("Users to be deleted: {}".format(delete_dic))
 
-  if confirm("Do you want to delete this users? (y/n) "):
+  # Ask for confirmation before actually deleting the users.
+  if confirm("\nDo you want to delete this users? (y/n): "):
     print("Deleting users...")
     url = 'https://' + server + ':9060/ers/config/internaluser/'
     for user in delete_dic:
       print("Deleting user {}...".format(user), end =" ")
       response = requests.delete(url + str(delete_dic[user]), headers=headers, auth=auth, verify=False)
       print(response.ok)
-
   else:
     print("Exiting without deleting. Bye!")
 
+# List was selected with arguments when the script was run.
 else:
-  print("Here is the list of configured users on ISE Server:")
-  print(user_list)
+  print("\nHere is the list of configured users on ISE Server:\n")
+    
+  # Print the names of the columns.
+  print(" ------------+-------------------------------------- ")
+  print ("| {:<10} | {:<36} |".format('USERNAME', 'ID'))
+  print(" ------------+-------------------------------------- ")
+  
+  # Print each data item.
+  for key, value in user_list.items():
+    print ("| {:<10} | {:<36} |".format(key, value))
+  
+  print(" ------------+-------------------------------------- ")
